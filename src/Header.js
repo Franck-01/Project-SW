60c40e527e35d217254160e4cf5436a676ba8e4f
Franco Acuña <franconacuna5701@gmail.com>
2021-11-27 17:49:20 -0300

    menu

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="images/SWTFA.png"/>
+                <h2 className="white">Escoje tus naves, inspecciona el arsenal y crea la flota mas poderosa que la galaxia haya visto</h2>
+            </div>
+        </header>
+    );
+}
+export default Header;
