0dd0c74cb97067499214ae76674f86dce214774e
Franco Acuña <franconacuna5701@gmail.com>
2021-12-05 19:20:53 -0300

    contador

--- src/Section.js
+++ src/Section.js
@@ -2,23 +2,59 @@ import {useState} from "react"
 
 const Section = () => {
 
+    const destInit = 0
+    const [dest, contadorA] = useState(destInit)
+    const CDestructores = () => {
+        console.log("Destructores clase Venator")
+        contadorA(dest + 1)
+    }
+    const destructores = () => {
+        contadorA(dest - 1)
+    }
     const cazInit = 0
-    const [caz, setContador] = useState(cazInit)
+    const [caz, contadorB] = useState(cazInit)
     const CCazas = () => {
         console.log("Cazas clase ARC-170")
-        setContador(caz + 30)
+        contadorB(caz + 30)
     }
     const cazas = () => {
-        setContador(caz - 30)
+        contadorB(caz - 30)
+    }
+    const bombInit = 0
+    const [bomb, contadorC] = useState(bombInit)
+    const CBombarderos = () => {
+        console.log("Bombarderos clase Ala-Y")
+        contadorC(bomb + 12)
+    }
+    const bombarderos = () => {
+        contadorC(bomb - 12)
+    }
+    const interInit = 0
+    const [inter, contadorD] = useState(interInit)
+    const CInterceptores = () => {
+        console.log("Interceptores clase Ala-V")
+        contadorD(inter + 10)
+    }
+    const interceptores = () => {
+        contadorD(inter - 10)
     }
 
     return (
         <section>
             <div className="section">
             <h1>Crea tu flota</h1>
-                <p>cantidad de cazas clase ARC-170 : {caz}</p>
+            <p>cantidad de destructores clase Venator : {dest}</p>
+                <button onClick={CDestructores}>aumentar</button>
+                <button onClick={destructores}>disminuir</button>
+            <p>cantidad de cazas clase ARC-170 : {caz}</p>
                 <button onClick={CCazas}>aumentar</button>
                 <button onClick={cazas}>disminuir</button>
+            <p>cantidad de bombarderos clase Ala-Y : {bomb}</p>
+                <button onClick={CBombarderos}>aumentar</button>
+                <button onClick={bombarderos}>disminuir</button>
+            <p>cantidad de interceptores clase Ala-V : {inter}</p>
+                <button onClick={CInterceptores}>aumentar</button>
+                <button onClick={interceptores}>disminuir</button>
             </div>
         </section>
         
